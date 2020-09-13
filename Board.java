51a9f73dc4eed9eeaed02ee9df19c1fd0a259a1b
Maximilien Breughe <mbreughe@gmail.com>
2020-09-13 10:33:35 -0500

    Implemented manhattan distance

--- Board.java
+++ Board.java
@@ -5,10 +5,39 @@
  **************************************************************************** */
 
 public class Board {
+    private int[][] mTiles;
+
+    private int getN() {
+        return mTiles.length;
+    }
+
+    private int goal(int i, int j) {
+        int goal = (1 + i * getN() + j);
+        // 0 needs to be the very last tile:
+        goal = goal % (getN() * getN());
+        return goal;
+    }
+
+    private int[] findTile(int x) {
+        int[] a = new int[2];
+        a[0] = a[1] = -1;
+        for (int i = 0; i < getN(); i++) {
+            for (int j = 0; j < getN(); j++) {
+                if (mTiles[i][j] == x) {
+
+                    a[0] = i;
+                    a[1] = j;
+                    return a;
+                }
+            }
+        }
+        return a;
+    }
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
+        mTiles = tiles;
     }
 
     // string representation of this board
@@ -23,12 +52,22 @@ public class Board {
 
     // number of tiles out of place
     public int hamming() {
+
         return -1;
     }
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
-        return -1;
+        int distance = 0;
+        for (int i = 0; i < getN(); i++) {
+            for (int j = 0; j < getN(); j++) {
+                int[] coords = findTile(goal(i, j));
+
+                distance += Math.abs(coords[0] - i) + Math.abs(coords[1] - j);
+            }
+        }
+
+        return distance;
     }
 
     // is this board the goal board?
