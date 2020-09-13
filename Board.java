661e11ae62e729244a644430e20e17053f75eed4
Maximilien Breughe <mbreughe@gmail.com>
2020-09-13 11:55:22 -0500

    Finished Board implementation

--- Board.java
+++ Board.java
@@ -4,11 +4,27 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.ArrayList;
+
+
 public class Board {
     private int[][] mTiles;
 
     private int getN() {
         return mTiles.length;
+    }
+
+    private Board swapTiles(int[] src, int[] dst) {
+        int[][] newTiles = new int[mTiles.length][];
+        for (int i = 0; i < mTiles.length; i++) {
+            newTiles[i] = mTiles[i].clone();
+        }
+        newTiles[dst[0]][dst[1]] = mTiles[src[0]][src[1]];
+        newTiles[src[0]][src[1]] = mTiles[dst[0]][dst[1]];
+        return new Board(newTiles);
     }
 
     private int goal(int i, int j) {
@@ -37,23 +53,41 @@ public class Board {
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
-        mTiles = tiles;
+        mTiles = new int[tiles.length][];
+        for (int i = 0; i < tiles.length; i++) {
+            mTiles[i] = tiles[i].clone();
+        }
     }
 
     // string representation of this board
     public String toString() {
-        return "unimplemented";
+        String repr = "" + getN() + "\n";
+        for (int i = 0; i < getN(); i++) {
+            for (int j = 0; j < getN(); j++) {
+                repr += "" + mTiles[i][j] + " ";
+            }
+            repr += "\n";
+        }
+
+        return repr;
     }
 
     // board dimension n
     public int dimension() {
-        return -1;
+        return getN();
     }
 
     // number of tiles out of place
     public int hamming() {
-
-        return -1;
+        int distance = 0;
+        for (int i = 0; i < getN(); i++) {
+            for (int j = 0; j < getN(); j++) {
+                if (mTiles[i][j] != goal(i, j)) {
+                    distance += 1;
+                }
+            }
+        }
+        return distance;
     }
 
     // sum of Manhattan distances between tiles and goal
@@ -77,13 +111,64 @@ public class Board {
 
     // does this board equal y?
     public boolean equals(Object y) {
-        return false;
+        if (this.getClass() != y.getClass()) {
+            return false;
+        }
+        Board other = (Board) y;
+
+        if (other.dimension() != dimension()) {
+            return false;
+        }
+
+        for (int i = 0; i < getN(); i++) {
+            for (int j = 0; j < getN(); j++) {
+                if (other.mTiles[i][j] != mTiles[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return null;
+        int[] coords = findTile(0);
+        int X = coords[0];
+        int Y = coords[1];
+
+        ArrayList<Board> neighbies = new ArrayList<Board>();
+
+        if (X - 1 > 0) {
+            int[] newCoords = new int[2];
+            newCoords[0] = X - 1;
+            newCoords[1] = Y;
+            neighbies.add(swapTiles(coords, newCoords));
+        }
+
+        if (X + 1 < getN()) {
+            int[] newCoords = new int[2];
+            newCoords[0] = X + 1;
+            newCoords[1] = Y;
+            neighbies.add(swapTiles(coords, newCoords));
+        }
+
+        if (Y - 1 > 0) {
+            int[] newCoords = new int[2];
+            newCoords[0] = X;
+            newCoords[1] = Y - 1;
+            neighbies.add(swapTiles(coords, newCoords));
+        }
+
+        if (Y + 1 < getN()) {
+            int[] newCoords = new int[2];
+            newCoords[0] = X;
+            newCoords[1] = Y + 1;
+            neighbies.add(swapTiles(coords, newCoords));
+        }
+
+        return neighbies;
     }
 
     // a board that is obtained by exchanging any pair of tiles
@@ -91,8 +176,39 @@ public class Board {
         return this;
     }
 
+    public static int[][] readTiles(String filename) {
+        In in = new In(filename);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                tiles[i][j] = in.readInt();
+            }
+        }
+        return tiles;
+    }
+
     // unit testing (not graded)
     public static void main(String[] args) {
+        int[][] tiles = readTiles(args[0]);
+        int[][] tiles_mod = new int[tiles.length][];
+        for (int i = 0; i < tiles.length; i++) {
+            tiles_mod[i] = tiles[i].clone();
+        }
+        tiles_mod[0][0] = 77;
+        Board x = new Board(tiles);
+        Board x_c = new Board(tiles);
+        Board y = new Board(tiles_mod);
 
+        StdOut.println("Manhattan: " + x.manhattan());
+        StdOut.println("Hamming: " + x.hamming());
+        StdOut.println("String Repr: " + x.toString());
+        StdOut.println("Equal to self? " + (x.equals(x_c)));
+        StdOut.println("Equal to small mod? " + (x.equals(y)));
+        StdOut.println("Neighbors:");
+
+        for (Board n : x.neighbors()) {
+            StdOut.println(n.toString());
+        }
     }
 }
